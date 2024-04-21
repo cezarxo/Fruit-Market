66ec948dcc4d257a43e7ab6676f6d6caecc8d051
cezarxo <aziz.ahmed@oxygenict.com>
2024-04-21 06:30:46 +0400

    adding animation for splash view

--- lib/features/splash/presentation/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/widgets/splash_view_body.dart
@@ -1,7 +1,33 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  AnimationController? animationController;
+  Animation<double>? fadingAnimation;
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 600));
+    fadingAnimation =
+        Tween<double>(begin: 0.2, end: 1).animate(animationController!);
+    animationController?.repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    animationController?.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,16 +37,18 @@ class SplashViewBody extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           const Spacer(),
-          const Text(
-            'Fruit Market',
-            style: TextStyle(
-              fontFamily: 'Poppins',
-              fontSize: 51,
-              fontWeight: FontWeight.bold,
-              color: Color(0xFFFFFFFF),
+          FadeTransition(
+            opacity: fadingAnimation!,
+            child: const Text(
+              'Fruit Market',
+              style: TextStyle(
+                fontFamily: 'Poppins',
+                fontSize: 51,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFFFFFFFF),
+              ),
             ),
           ),
-        
           Image.asset('assets/images/splash_view_image.png')
         ],
       ),
